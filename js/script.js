3cc1bcb6030436ecc4f1862c5b39297c52762147
NikitaKrulov <117915940+NikitaKrulov@users.noreply.github.com>
2024-01-26 20:13:29 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,91 @@
+
+
+var SwiperNewsPage = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  spaceBetween: 50,
+  loop: true,
+  navigation: {
+      nextEl: '.question__swipperBtn-rightBtn',
+      prevEl: '.question__swipperBtn-leftBtn',
+  },
+});
+
+$('.news__action-btn').on('click', function() {
+  for (var i = 0; i < 3; i++) {
+    const newsCard__two = `
+      <a href="#" class="newsCard">
+        <div class="newsCard__img">
+          <img src="./img/CardBG1.png" alt="CardBG">
+        </div>
+        <div class="newsCard__title">
+          <p class="newsCard__title-date">12 ноября 2023</p>
+          <p class="newsCard__title-head">Поздравляем! Новое свидетельство от РосПатента!</p>
+          <p class="newsCard__title-text">Научный коллектив ИРБиС, состоящий из студентов и преподавателей, разработал интересный сервис, позволяющий при помощи программы быстро и качественно определять подлинность денежной банкноты.</p>
+          <p class="newsCard__footer">Читать далее<span class="icon-arrow"></span></p>
+        </div>
+      </a>`;
+
+    $('.news__two .news__content').append(newsCard__two);
+  }
+});
+
+
+$(document).ready(function () {
+  updateLastCardClass();
+
+  $(window).on('resize', function () {
+    updateLastCardClass();
+  });
+
+  $('.news__action-btn').on('click', function () {
+    updateLastCardClass();
+  });
+
+  function updateLastCardClass() {
+    var screenWidth = $(window).width();
+
+    if (screenWidth < 768) {
+      $('.news__two .newsCard').removeClass('newsCard--last');
+      return; 
+    }
+    if (screenWidth > 993) {
+      $('.news__two .newsCard').removeClass('newsCard--last');
+      return; 
+    }
+    var newsCardCount = $('.news__two .newsCard').length;
+
+    $('.news__two .newsCard').removeClass('newsCard--last');
+
+    if (screenWidth < 993 && newsCardCount % 2 === 0) {
+      $('.news__two .newsCard:nth-last-child(-n+2)').addClass('newsCard--last');
+    } else if (screenWidth < 993 && newsCardCount % 2 !== 0) {
+      $('.news__two .newsCard:last').addClass('newsCard--last');
+    }
+  }
+});
+
+
+
+
+
+
+// main page ----------------------------
+const header = document.querySelector('.header');
+const navContainer = document.querySelector('.navContainer');
+
+const updateNavMargin = () => {
+  if (window.innerWidth <= 992) {
+    const headerMarginLeft = parseInt(window.getComputedStyle(header).marginLeft, 10);
+    const headerMarginLeft1 = Math.max(headerMarginLeft - 20, 0); 
+    navContainer.style.marginLeft = headerMarginLeft1 + 'px';
+  } else {
+    navContainer.style.marginLeft = '0';
+  }
+};
+
+window.addEventListener('resize', updateNavMargin);
+updateNavMargin();
+
 $(document).ready(function () {
   $('.burger__menu-icon').click(function (event) {
     event.stopPropagation();
@@ -29,7 +117,73 @@ $(document).ready(function () {
     event.stopPropagation();
   });
 
+  $(window).resize(function () {
+    if ($(window).width() > 993) {
+      $('#burger-dropdown').removeClass('open');
+      $('.burger__menu-icon').removeClass('open');
+      $('.burger__menu').removeClass('open');
+      $('html, body').removeClass('overflow-hidden');
+    }
+  });
 });
+const videoContainersNews = document.querySelectorAll('.questionSwiper__slide-video');
+
+videoContainersNews.forEach(videoContainer => {
+  const video = videoContainer.querySelector('video');
+  const playPauseBtn = videoContainer.querySelector('button');
+
+  videoContainer.addEventListener('mouseenter', () => {
+    playPauseBtn.style.opacity = '1';
+  });
+
+  videoContainer.addEventListener('mouseleave', () => {
+    if (!video.paused) {
+      playPauseBtn.style.opacity = '0';
+    }
+  });
+
+  playPauseBtn.addEventListener('click', () => {
+    if (video.paused) {
+      video.play();
+    } else {
+      video.pause();
+    }
+  });
+
+  video.addEventListener('ended', () => {
+    playPauseBtn.style.opacity = '1';
+  });
+});
+
+const videoContainers = document.querySelectorAll('.questionSwiper__slide-video');
+const videoContainer = document.getElementById('videoContainer');
+const video = document.getElementById('specialtiesVideo');
+const playPauseBtn = document.getElementById('playPauseBtn');
+
+videoContainer.addEventListener('mouseenter', () => {
+  if (!video.paused) {
+    playPauseBtn.style.opacity = '1';
+  }
+});
+
+videoContainer.addEventListener('mouseleave', () => {
+  if (!video.paused) {
+    playPauseBtn.style.opacity = '0';
+  }
+});
+
+playPauseBtn.addEventListener('click', () => {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+});
+
+video.addEventListener('ended', () => {
+  playPauseBtn.style.opacity = '1';
+});
+
 
 
 
@@ -54,34 +208,14 @@ document.querySelectorAll(".hero__pagination.swiper-pagination-bullet").forEach(
 });
 
 function updateImage(slideIndex) {
-  const imgContainer = document.querySelector(".hero__img-container");
-  const imagePaths = [
-    "./img/Intersect.png",
-    "./img/CardBG1.png",
-    "./img/CardBG2.png",
-    "./img/CardBG3.png",
-  ];
-
-  if (slideIndex >= 0 && slideIndex < imagePaths.length) {
-    imgContainer.style.backgroundImage = `url("${imagePaths[slideIndex]}")`;
-  }
+  const image = document.getElementById("image0_68_1872");
+  const newImagePath = `./img/bg${slideIndex}.png`; // изображения  bg0.png bg1.png bg2.png
+  image.setAttribute("xlink:href", newImagePath);
 }
 
 
-const header = document.querySelector('.header');
-const navContainer = document.querySelector('.navContainer');
 
-const updateNavMargin = () => {
-  if (window.innerWidth <= 992) {
-    const headerMarginLeft = window.getComputedStyle(header).marginLeft;
-    navContainer.style.marginLeft = headerMarginLeft;
-  } else {
-    navContainer.style.marginLeft = '0';
-  }
-};
 
-window.addEventListener('load', updateNavMargin);
-window.addEventListener('resize', updateNavMargin);
 
   // SwiperNews -----------
   
@@ -93,7 +227,7 @@ window.addEventListener('resize', updateNavMargin);
         slidesPerView: 3.2,
         slidesPerGroup: 1,
       },
-      768: {
+      769: {
         slidesPerView: 2.2, 
         slidesPerGroup: 1,
       },
@@ -126,7 +260,7 @@ window.addEventListener('resize', updateNavMargin);
   const SwiperReviews = new Swiper(".swiperReviews", {
     spaceBetween: 15,
     breakpoints: {
-      768: {
+      769: {
         slidesPerView: 2.2,
         slidesPerGroup: 1, 
       },
@@ -162,40 +296,275 @@ jQuery(document).ready(function($){
     });
 });
 
-
-const videoContainer = document.getElementById('videoContainer');
-const video = videoContainer.querySelector('video');
-const playPauseBtn = videoContainer.querySelector('#playPauseBtn');
-
-playPauseBtn.style.opacity = '1';
-
-// при наведении
-videoContainer.addEventListener('mouseenter', () => {
-    playPauseBtn.style.opacity = '1';
-});
-
-// Скрыть кнопку 
-videoContainer.addEventListener('mouseleave', () => {
-    if (video.paused) {
-        playPauseBtn.style.opacity = '1';
-    } else {
-        playPauseBtn.style.opacity = '0';
+  //  all ----------------------------
+  function expandCardCircle(event, element) {
+    let timeoutId;
+  
+    const existingCircle = element.querySelector('.circle');
+    if (existingCircle) {
+      return;
     }
-});
-
-video.addEventListener('play', () => {
-    playPauseBtn.style.opacity = '0';
-});
-
-video.addEventListener('pause', () => {
-    playPauseBtn.style.opacity = '1';
-});
-
-playPauseBtn.addEventListener('click', function() {
-    if (video.paused) {
-        video.play();
-    } else {
-        video.pause();
+  
+    const circle = document.createElement('div');
+    circle.classList.add('circle');
+    element.appendChild(circle);
+  
+    const rect = element.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+  
+    circle.style.left = x + 'px';
+    circle.style.top = y + 'px';
+    circle.style.transform = 'translate(-50%, -50%) scale(0)';
+    void circle.offsetWidth;
+  
+    circle.style.width = circle.style.height = Math.max(rect.width, rect.height) * 3 + 'px';
+    circle.style.transform = 'translate(-50%, -50%) scale(1)';
+  
+    document.addEventListener('mousemove', checkCardCursor);
+    element.addEventListener('mouseleave', handleMouseLeave);
+  
+    function checkCardCursor(event) {
+      if (
+        event.clientX < rect.left ||
+        event.clientX > rect.right ||
+        event.clientY < rect.top ||
+        event.clientY > rect.bottom
+      ) {
+        resetCardCircle(element);
+        document.removeEventListener('mousemove', checkCardCursor);
+      }
     }
+  
+    function handleMouseLeave() {
+      resetCardCircle(element);
+      element.removeEventListener('mouseleave', handleMouseLeave);
+    }
+  
+    function resetCardCircle(element) {
+      const circle = element.querySelector('.circle');
+      if (circle) {
+        circle.style.transform = 'translate(-50%, -50%) scale(0)';
+        timeoutId = setTimeout(() => {
+          circle.remove();
+        }, 200); 
+      }
+    }
+  }
+  
+  const cards = document.querySelectorAll('.about__cardContainer-card');
+  cards.forEach(card => {
+    card.addEventListener('mouseenter', (event) => expandCardCircle(event, card));
+    card.addEventListener('mouseleave', () => {
+      resetCardCircle(card);
+    });
+    card.addEventListener('mousemove', (event) => {
+      event.stopPropagation();
+    });
+  });
+  
+  const cardsCollege = document.querySelectorAll('.cardCollege');
+  cardsCollege.forEach(card => {
+    card.addEventListener('mouseenter', (event) => expandCardCircle(event, card));
+    card.addEventListener('mouseleave', () => {
+      resetCardCircle(card);
+    });
+    card.addEventListener('mousemove', (event) => {
+      event.stopPropagation();
+    });
+  });
+  
+
+  //  grey  ----------------------------
+let timeoutIdBtn;
+
+function expandBtnCircle(event, element) {
+  clearTimeout(timeoutIdBtn);
+
+  const existingCircle = element.querySelector('.circleBtn');
+  if (existingCircle) {
+    return;
+  }
+
+  const circle = document.createElement('div');
+  circle.classList.add('circleBtn');
+  element.appendChild(circle);
+
+  const rect = element.getBoundingClientRect();
+  const x = event.clientX - rect.left;
+  const y = event.clientY - rect.top;
+
+  circle.style.left = x + 'px';
+  circle.style.top = y + 'px';
+  circle.style.transform = 'translate(-50%, -50%) scale(0)';
+  void circle.offsetWidth;
+
+  circle.style.width = circle.style.height = Math.max(rect.width, rect.height) * 3 + 'px';
+  circle.style.transform = 'translate(-50%, -50%) scale(1)';
+
+  document.addEventListener('mousemove', checkBtnCursor);
+
+  function checkBtnCursor(event) {
+    if (
+      event.clientX < rect.left ||
+      event.clientX > rect.right ||
+      event.clientY < rect.top ||
+      event.clientY > rect.bottom
+    ) {
+      resetBtnCircle(element);
+      document.removeEventListener('mousemove', checkBtnCursor);
+    }
+  }
+}
+
+function resetBtnCircle(element) {
+  const circle = element.querySelector('.circleBtn');
+  if (circle) {
+    circle.style.transform = 'translate(-50%, -50%) scale(0)';
+    timeoutIdBtn = setTimeout(() => {
+      circle.remove();
+    }, 200);
+  }
+}
+
+const greyBtns = document.querySelectorAll('.grayBtn');
+greyBtns.forEach(btn => {
+  btn.addEventListener('mouseenter', (event) => expandBtnCircle(event, btn));
+  btn.addEventListener('mouseleave', () => {
+    resetBtnCircle(btn);
+  });
+  btn.addEventListener('mousemove', (event) => {
+    event.stopPropagation();
+  });
 });
+
+
+  //  pink ----------------------------
+let timeoutIdBtnPink;
+
+function expandBtnPinkCircle(event, element) {
+  clearTimeout(timeoutIdBtnPink);
+
+  const existingCircle = element.querySelector('.circleBtn-pink');
+  if (existingCircle) {
+    return;
+  }
+
+  const circle = document.createElement('div');
+  circle.classList.add('circleBtn-pink');
+  element.appendChild(circle);
+
+  const rect = element.getBoundingClientRect();
+  const x = event.clientX - rect.left;
+  const y = event.clientY - rect.top;
+
+  circle.style.left = x + 'px';
+  circle.style.top = y + 'px';
+  circle.style.transform = 'translate(-50%, -50%) scale(0)';
+  void circle.offsetWidth;
+
+  circle.style.width = circle.style.height = Math.max(rect.width, rect.height) * 3 + 'px';
+  circle.style.transform = 'translate(-50%, -50%) scale(1)';
+
+  document.addEventListener('mousemove', checkBtnPinkCursor);
+
+  function checkBtnPinkCursor(event) {
+    if (
+      event.clientX < rect.left ||
+      event.clientX > rect.right ||
+      event.clientY < rect.top ||
+      event.clientY > rect.bottom
+    ) {
+      resetBtnPinkCircle(element);
+      document.removeEventListener('mousemove', checkBtnPinkCursor);
+    }
+  }
+}
+
+function resetBtnPinkCircle(element) {
+  const circle = element.querySelector('.circleBtn-pink');
+  if (circle) {
+    circle.style.transform = 'translate(-50%, -50%) scale(0)';
+    timeoutIdBtnPink = setTimeout(() => {
+      circle.remove();
+    }, 200);
+  }
+}
+
+const pinkBtns = document.querySelectorAll('.pinkBtn');
+pinkBtns.forEach(btn => {
+  btn.addEventListener('mouseenter', (event) => expandBtnPinkCircle(event, btn));
+  btn.addEventListener('mouseleave', () => {
+    resetBtnPinkCircle(btn);
+  });
+  btn.addEventListener('mousemove', (event) => {
+    event.stopPropagation();
+  });
+});
+
+// deatil ----------------------------------------
+
+let timeoutIdBtnDetail;
+
+function expandBtnDetailCircle(event, element) {
+  clearTimeout(timeoutIdBtnDetail);
+
+  const existingCircle = element.querySelector('.circleBtn-detailBtn');
+  if (existingCircle) {
+    return;
+  }
+
+  const circle = document.createElement('div');
+  circle.classList.add('circleBtn-detailBtn');
+  element.appendChild(circle);
+
+  const rect = element.getBoundingClientRect();
+  const x = event.clientX - rect.left;
+  const y = event.clientY - rect.top;
+
+  circle.style.left = x + 'px';
+  circle.style.top = y + 'px';
+  circle.style.transform = 'translate(-50%, -50%) scale(0)';
+  void circle.offsetWidth;
+
+  circle.style.width = circle.style.height = Math.max(rect.width, rect.height) * 3 + 'px';
+  circle.style.transform = 'translate(-50%, -50%) scale(1)';
+
+  document.addEventListener('mousemove', checkBtnDetailCursor);
+
+  function checkBtnDetailCursor(event) {
+    if (
+      event.clientX < rect.left ||
+      event.clientX > rect.right ||
+      event.clientY < rect.top ||
+      event.clientY > rect.bottom
+    ) {
+      resetBtnDetailCircle(element);
+      document.removeEventListener('mousemove', checkBtnDetailCursor);
+    }
+  }
+}
+
+function resetBtnDetailCircle(element) {
+  const circle = element.querySelector('.circleBtn-detailBtn');
+  if (circle) {
+    circle.style.transform = 'translate(-50%, -50%) scale(0)';
+    timeoutIdBtnDetail = setTimeout(() => {
+      circle.remove();
+    }, 200);
+  }
+}
+
+const detailBtns = document.querySelectorAll('.cardCollege__content-detailBtn');
+detailBtns.forEach(btn => {
+  btn.addEventListener('mouseenter', (event) => expandBtnDetailCircle(event, btn));
+  btn.addEventListener('mouseleave', () => {
+    resetBtnDetailCircle(btn);
+  });
+  btn.addEventListener('mousemove', (event) => {
+    event.stopPropagation();
+  });
+});
+
+
 
